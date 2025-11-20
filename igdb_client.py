3c964759abd733c76c8ffc783a4166f6184707f4
Samuel Valls <vallsfs@outlook.com>
2025-11-19 16:19:22 -0800

    "deleted files"

--- igdb_client.py
+++
@@ -1,9 +0,0 @@
-import os
-from igdb.wrapper import IGDBWrapper
-from auth import get_app_token
-
-CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
-
-def wrapper():
-    request = IGDBWrapper(CLIENT_ID, get_app_token())
-    return request
